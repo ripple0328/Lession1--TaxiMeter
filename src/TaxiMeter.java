ded3b714d04fc52b77963275aaad21d99de60bbf
Qingbo ZHANG <ripple0328@gmail.com>
2013-09-21 23:22:19 +0800

    finish unit test of waitTime distance and round function

--- src/TaxiMeter.java
+++ src/TaxiMeter.java
@@ -6,8 +6,31 @@
  * To change this template use File | Settings | File Templates.
  */
 public class TaxiMeter {
-    static public double calculateTaxiFee(double distance,double waitTime){
-           return 6;
+		public static final double FLAG_KM = 2.0;
+    public static final double PER_PRICE = 0.8;
+    public static final double FLAG_PRICE = 6.0;
+    public static final double PER_WAIT_PRICE = 0.25;
+    public static final double  BASE_KM = 8.0;
+    public static final double  LONG_DISTANCE_EXTRA_FEE = 0.5;
+		public static final double PRECISION = 0.001;
+
+    static public double distanceFee(double distance){
+				double distanceFee = 0;
+				if (distance <  (FLAG_KM + PRECISION)) {
+						distanceFee = FLAG_PRICE;
+				} else if (distance < (BASE_KM + PRECISION)){
+						distanceFee  = FLAG_PRICE + (distance - FLAG_KM)* PER_PRICE;
+				} else {
+						distanceFee = FLAG_PRICE + (distance - FLAG_KM)* (PER_PRICE * (1 + LONG_DISTANCE_EXTRA_FEE));
+				}
+				return distanceFee;
     }
+		static public int round(double rawTaxiFee){
+				return (int) Math.round(rawTaxiFee);
+		}
+
+		static public double waitFee(double minute){
+				return minute * PER_WAIT_PRICE;
+		}
 
 }
--- test/TaxiMeterTest.java
+++ test/TaxiMeterTest.java
@@ -12,22 +12,55 @@ import static junit.framework.Assert.assertEquals;
 public class TaxiMeterTest {
 
     public TaxiMeter TaxiFee;
+		public static final double PRECISION = 0.001;
+		
     @Before
     public void setUp() throws Exception{
         TaxiFee = new TaxiMeter();
     }
-		// no wait time
+		// Flat price no wait time
     @Test
     public void should_return_6_when_distance_is_1_km() throws Exception {
-        assertEquals(6.0,TaxiFee.calculateTaxiFee(1,0));
+        assertEquals(6,TaxiFee.distanceFee(1),PRECISION);
     }
 		@Test
     public void should_return_6_when_distance_is_2_km() throws Exception {
-        assertEquals(6.0,TaxiFee.calculateTaxiFee(2,0));
+        assertEquals(6,TaxiFee.distanceFee(2),PRECISION);
     }
 		@Test
     public void should_return_6_when_any_distance_less_than_2km() throws Exception {
-        assertEquals(6.0,TaxiFee.calculateTaxiFee(1.5,0));
+        assertEquals(6,TaxiFee.distanceFee(1.5),PRECISION);
     }
+		// 2-8km no wait times
 		@Test
+    public void should_return_6_point_8_when_distance_is_3km() throws Exception {
+        assertEquals(6.8,TaxiFee.distanceFee(3),PRECISION);
+    }
+		// greater than 8km no wait time 
+		@Test
+    public void should_return_10_point_8_when_distance_is_8km() throws Exception {
+        assertEquals(10.8,TaxiFee.distanceFee(8),PRECISION);
+    }
+
+		@Test
+    public void should_return_8_when_distance_is_10km() throws Exception {
+        assertEquals(15.6,TaxiFee.distanceFee(10),PRECISION);
+    }
+
+		// rount price
+		@Test
+		public void should_round_fee() throws Exception {
+        assertEquals(6,TaxiFee.round(6.4));
+				assertEquals(7,TaxiFee.round(6.5));
+    }
+
+		// wait time
+		@Test
+		public void waitTimeFee() throws Exception {
+				assertEquals(1,TaxiFee.waitFee(4),PRECISION);
+		}
+
+		// integrate test
+		
+		
 }
