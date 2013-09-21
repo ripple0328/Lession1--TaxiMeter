37f57659e49ca4203699b5f0116049026a857cd7
Qingbo ZHANG <ripple0328@gmail.com>
2013-09-21 21:23:11 +0800

    first commit

---
+++ test/TaxiMeterTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: qingbo
+ * Date: 13-9-21
+ * Time: PM8:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaxiMeterTest {
+
+}
